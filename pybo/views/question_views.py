a010f6513942eb3f0bc37607694635fb4425cb00
ss820938ss <ss820938ss@gmail.com>
2021-08-20 16:48:54 +0900

    웹사이트 기초 틀 작성중

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 
 from pybo.models import Question
-from ..forms import QuestionForm
+from ..forms import QuestionForm, AnswerForm
 from datetime import datetime
 
 from flask import Blueprint, render_template, request, url_for
@@ -20,8 +20,9 @@ def _list():
 
 @bp.route('/detail/<int:question_id>/')
 def detail(question_id):
+    form = AnswerForm()
     question = Question.query.get_or_404(question_id)
-    return render_template('question/question_detail.html', question=question)
+    return render_template('question/question_detail.html', question=question, form=form)
 
 
 @bp.route('/create/', methods=('GET', 'POST'))
