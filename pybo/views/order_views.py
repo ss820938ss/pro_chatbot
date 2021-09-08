98c3a44e14cd75773bc0805aff1ad2a3dc8d8053
ss820938ss <ss820938ss@gmail.com>
2021-09-08 14:16:58 +0900

    갯수 중첩 작성중

--- pybo/views/order_views.py
+++ pybo/views/order_views.py
@@ -3,6 +3,9 @@ import sys
 import requests
 from flask import *
 from flask_wtf import form
+from sqlalchemy import distinct
+from sqlalchemy.sql.functions import count
+from sqlalchemy.util import counter
 
 from pybo.views.auth_views import login_required
 from pybo.models import Products, Categories, Users
@@ -14,6 +17,7 @@ bp = Blueprint('order', __name__, url_prefix='/order')
 
 # 장바구니 목록
 user = None
+cart = None
 
 
 @bp.route('/order/', methods=('GET', 'POST'))
@@ -21,6 +25,7 @@ user = None
 def _order(categories=None):
     if request.method == 'GET':
         global user
+        global cart
 
         user = db.session.query(Users).filter_by(email=session.get('user_id')).first()
 
@@ -41,13 +46,14 @@ def _order(categories=None):
         #cart_list = Cart.query.order_by(Cart.userId)
 
         total_price = int(cart.price * cart.qty)
+        total_qty = cart.qty
 
-        print("========111", cart, file=sys.stderr)
+        print("========111", total_qty, file=sys.stderr)
         print("========222", total_price, file=sys.stderr)
     else:
         flash('물건을 담는데 실패했습니다.')
 
-    return render_template('order/order.html', form=form, products=products, user=user, cart=cart, cart_list=cart_list, categories=categories, total_price=total_price)
+    return render_template('order/order.html', form=form, products=products, user=user, cart=cart, cart_list=cart_list, categories=categories, total_price=total_price, total_qty=total_qty)
 
 
 @bp.route('/payment/', methods=('GET', 'POST'))
