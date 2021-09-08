bdd1a2677511570925be3b5b7a58cb5ef0150b58
ss820938ss <ss820938ss@gmail.com>
2021-09-08 18:26:05 +0900

    결제 눌렀을때 카트는 비워지는데 오류뜸

--- pybo/views/order_views.py
+++ pybo/views/order_views.py
@@ -32,8 +32,9 @@ def _order(categories=None):
         qty = request.args.get('qty')
 
         products = db.session.query(Products).filter_by(productId=productId).first()
-        price_total = db.session.query(Cart.price)  # 합계
-        qty_total = db.session.query(Cart).filter(Cart.productId).count()  # 갯수 합계...인데 지금 추가한 항목이 동시에 올라감
+        price_total = db.session.query(Cart.price).order_by().all()  # 합계
+
+        qty_total = db.session.query(Cart.price).filter(Cart.userId).count()  # 갯수 합계...인데 지금 추가한 항목이 동시에 올라감
 
         categories = db.session.query(Categories).filter_by(name=name).first()
 
@@ -42,11 +43,12 @@ def _order(categories=None):
         db.session.add(cart)
         db.session.commit()
 
-        cart_list = db.session.query(Cart).filter_by(userId=user.userId).all()
+        # cart_list = db.session.query(Cart).filter_by(userId=user.userId).all()
+        cart_list = Cart.query.order_by(Cart.userId).all()
 
-        #cart_list = Cart.query.order_by(Cart.userId)
-
-        print("========111", price_total, file=sys.stderr)
+        print("========111", cart, file=sys.stderr)
+        print("========222", price_total, file=sys.stderr)
+        print("========333", cart_list, file=sys.stderr)
     else:
         flash('물건을 담는데 실패했습니다.')
 
@@ -56,5 +58,14 @@ def _order(categories=None):
 @bp.route('/payment/', methods=('GET', 'POST'))
 @login_required
 def payment():
-    return render_template('order/payment.html')
+    if request.method == 'GET':
+        global cart
+        cart = db.session.query(Cart).first()
+        db.session.delete(cart)
+        db.session.commit()
+        return redirect(url_for('order.payment'))
+    else:
+        flash('결제 중 오류가 발생했습니다.')
+
+    return render_template('order/payment.html', cart=cart)
 
